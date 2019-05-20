86e62f665b2b5cfad84c86001b625b37049a9fa3
WiseEyeKOSO <36448457+WiseEyeKOSO@users.noreply.github.com>
2019-05-20 23:55:24 +0200

    GameIds added

--- apps/myrelease/src/tcpserver.erl
+++ apps/myrelease/src/tcpserver.erl
@@ -8,7 +8,7 @@
 -record(pos,{side,cord = 50}).
 -record(player,{name,from,position}).
 -record(game,{player1,player2}).
--record(state, {users = [],games = []}).
+-record(state, {current = 0,users = [],games = #{},ids = #{}}).
 
 start(Name) ->
    start_link(Name,5).
@@ -29,7 +29,7 @@ init(_) ->
                 {error,Reason}
         end,
     start_servers(5,LS),
-    {ok, #state{users = []}}.
+    {ok, #state{}}.
 
 find_user([]) ->
   no_user;
@@ -47,6 +47,8 @@ create_game({{Name1,{Pid1,_}},Side1},{{Name2,{Pid2,_}},Side2}) ->
   P2 = #player{name = Name2,from = Pid2,position = create_position(Side2)},
   #game{player1 = P1,player2 = P2}.
 
+get_opponent_pid(Pid,#game{player1 = #player{from = Pid},player2 = #player{from = Res}}) -> Res;
+get_opponent_pid(Pid,#game{player1 = #player{from = Res},player2 = #player{from = Pid}}) -> Res.
 
 handle_call(stop, _From, State) ->
    {stop, normal, stopped, State};
@@ -55,6 +57,8 @@ handle_call(Request, From, State) ->
     {Reply,NewState} =
         case Request of
             {message,register,UserName} ->
+
+              io:format("i am here ~p",[UserName]),
 
               Users = State#state.users,
               NewUser = {UserName,From},
@@ -68,9 +72,19 @@ handle_call(Request, From, State) ->
                   init_game(User,up),
                   init_game(NewUser,down),
                   NewGame = create_game({User,up},{NewUser,down}),
-                  {ok,State#state{users = Rest,games = [NewGame | Games]}}
-              end
-  
+                  GameId = State#state.current,
+                  {_,{Pid1,_}} = NewUser,
+                  {_,{Pid2,_}} = User,
+                  io:format("Found ~p",[GameId]),
+                  {ok,State#state{current = GameId + 1,users = Rest,games = maps:put(GameId,NewGame,Games),ids = maps:put(Pid1,GameId,maps:put(Pid2,GameId,State#state.ids))}}
+              end;
+            {message,move,Pos} ->
+              {Pd,_} = From,
+              GameId = maps:get(Pd,State#state.ids),
+              Game = maps:get(GameId,State#state.games),
+              OppPd = get_opponent_pid(Pd,Game),
+              OppPd ! {from_server,move_req,Pos},
+              {ok,State}
         end,
     {reply, Reply, NewState}.
 
@@ -94,14 +108,34 @@ start_servers(Num,LS) ->
   register(Nm,spawn(?MODULE,tcpserver,[LS,Nm])),  
   start_servers(Num-1,LS).
 
+rev (<<>>, Acc) -> Acc;
+rev (<<H:1/binary, Rest/binary>>, Acc) ->
+    rev(Rest, <<H/binary, Acc/binary>>).
+
+fetch_jsons(<<"}", Tail/binary>>, Accu,Res,1) ->
+  Reversed = rev(<<<<"}">>/binary , Accu/binary>> , <<>>),
+  R = [Reversed | Res],
+  fetch_jsons(Tail,<<>>,R,0);
+fetch_jsons(<<"}", Tail/binary>>, Accu,Res,Lvl) ->
+  fetch_jsons(Tail,<<<<"}">>/binary , Accu/binary>>,Res,Lvl - 1); 
+fetch_jsons(<<"{", Tail/binary>>, Accu,Res,Lvl) ->
+  fetch_jsons(Tail,<<<<"{">>/binary , Accu/binary>>,Res,Lvl + 1);    
+fetch_jsons(<<C, Tail/binary>>, Accu,Res,Lvl) ->
+  fetch_jsons(Tail,<<<<C>>/binary , Accu/binary>>,Res,Lvl);
+fetch_jsons(_, _,Res,_) ->
+  Res.
+
 process(Decoded) ->
     #{<<"tp">> := Tp,<<"content">> := Ct} = Decoded,
 
     case Tp of
         <<"register">> ->
-            io:format("Content -> ~p",[Ct]),
-            #{<<"name">> := UserName} = Ct,
-            {message,register,UserName}
+          io:format("Content -> ~p",[Ct]),
+          #{<<"name">> := UserName} = Ct,
+          {message,register,UserName};
+        <<"move_req">> ->
+          #{<<"pos">> := Pos} = Ct,
+          {message,move,Pos}
     end.        
     
 
@@ -112,10 +146,14 @@ loop(S,Nm) ->
         Resp = jsx:encode(#{<<"tp">> => <<"game_init">>,<<"body">> => #{<<"side">> => atom_to_binary(Side,latin1)}}),
         gen_tcp:send(S,Resp),
         loop(S,Nm);
+    {from_server,move_req,Pos} ->
+        Resp = jsx:encode(#{<<"tp">> => <<"move_req">>,<<"body">> => #{<<"pos">> => Pos}}),
+        gen_tcp:send(S,Resp),
+        loop(S,Nm);
     {tcp,S,Data} ->
-        Decoded = jsx:decode(Data,[return_maps]),
-        Answer = process(Decoded),
-        gen_server:call(srv,Answer), 
+        Jsons = fetch_jsons(Data,<<>>,[],0),
+        Messages = lists:map(fun (X) -> process(jsx:decode(X,[return_maps])) end,Jsons),
+        lists:foreach(fun(Answer) -> gen_server:call(srv,Answer) end,Messages), 
         loop(S,Nm);
     {tcp_closed,S} ->
         io:format("Socket ~w closed [~w]~n",[S,self()]),
